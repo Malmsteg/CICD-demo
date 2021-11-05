fd63b2b56f45d7ad40f47078b9309c3edc120b3a
Malmsteg <73699667+Malmsteg@users.noreply.github.com>
2021-11-05 11:09:16 +0100

    Updated the program again
    smth.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace CICD_demo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World2!");
         }
     }
 }
